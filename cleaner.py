e72d71448be805140a5e2e91f7ca16aa7a5f00ad
Endo_Hizumi <endohizumi0114@gmail.com>
2020-02-20 17:39:18 +0900

    WIP: cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,27 +1,13 @@
-import requests
 import json
 import time
+from slack_api import SlackApi
 
+with open('config.json') as config_file:
+    api = SlackApi(json.load(config_file))
 
-with open("config.json") as config_file:
-    config = json.load(config_file)
-token = config['token']
-channel = config.get['channel_id']
+history = api.history()
 
-
-
-
-for m in data["messages"]:
-    delete_params['ts'] = m["ts"]
-    enc_params = urllib.urlencode(delete_params)
-
-    req = urllib2.Request(delete_url)
-    req.add_header()
-    req.add_data(enc_params)
-
-    res = urllib2.urlopen(req)
-
-    body = res.read()
-    print(body)
+for m in history[0]['messages']:
+    api.delete(int(m['ts']))
     # 連続で送りすぎるとエラーになるので1秒待機
     time.sleep(1)
